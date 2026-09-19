53b64559830fe93e39db60a67dc9be2d884f0e7e
Galaxy <74384334+GalaxyM4@users.noreply.github.com>
2026-09-19 11:56:43 -0500

    feat: fav icon added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
+
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.ico';
 	import Navbar from '$lib/components/navbar.svelte';
 	import { ModeWatcher } from 'mode-watcher';
 	import '../app.css';
@@ -8,9 +9,16 @@
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="manifest" href="site.webmanifest">
+</svelte:head>
 
-<ModeWatcher />
+
+<ModeWatcher disableTransitions={false} />
 <Navbar />
 
 <main class="container mx-auto p-4">
